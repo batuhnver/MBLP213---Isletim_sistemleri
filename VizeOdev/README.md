e9cf4e441af528a26b1ff5786b1bc8660efc01a1
batuhnver <ark0ntos12@gmail.com>
2020-12-01 01:38:47 +0300

    VizeOdev

---
+++ VizeOdev/README.md
@@ -0,0 +1,2 @@
+#MBLP213-İşletim Sistemleri Vize Ödevi
+ -**Wİndows işletim sistemi üzerinde Port ve servis ilişkisini gösterip bir dosyaya aktarılmasını sağlayan  powershell script**
---
+++ VizeOdev/Vize.ps1
@@ -0,0 +1,40 @@
+﻿function arrayFilterSpace {
+    param($array)
+    $result = @()
+    for($i = 0; $i -lt $array.length; $i++) {
+        if ($array[$i]) {
+            $result += $array[$i];
+        }
+    }
+    return $result
+}
+
+$output = ""
+$i = 0
+foreach($item in Get-Process) {
+   $find = netstat -ano | findstr $item.Id 
+
+   if ($find) {
+        $result = arrayFilterSpace("$find".Split(' '));
+        $port = $result[1].Substring($result[1].lastIndexOf(':')+1)
+        
+        $name = $item.Name
+        $path = $item.Path
+        $protocol = $result[0]
+
+        $data = "Servis Adı: $name`r`n"
+        $data += "Port: $port`r`n"
+        $data += "Protokol: $protocol`r`n"
+        $data += "Path: $path`r`n`r`n"
+        $output += $data
+        $data
+        $i++
+    }
+}
+
+$totalText = "`r`nToplamda Bulunan: $i"
+$output += $totalText
+$totalText
+
+cd ~\Desktop
+$output | Out-File -FilePath ./output.txt
